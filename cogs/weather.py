7e9955201b1b67bd8e4783ea3e7988af7a0ad74b
Oscie57 <hudso009@gmail.com>
2021-06-25 17:22:29 +0100

    Update weather.py

--- cogs/weather.py
+++ cogs/weather.py
@@ -41,7 +41,7 @@ class Weather(commands.Cog):
         embed = discord.Embed(color=0x7289DA, title="Forecast Update")
 
         for forecast in weather.forecasts:
-            embed.add_field(name=forecast.day, value=f"{forecast.sky_text} | {forecast.temperature}°C\nLow: {forecast.low}°C | High: {forecast.high}°C\nPrecip: {forecast.precip}", inline=True)
+            embed.add_field(name=forecast.day, value=f"`Sky: `{forecast.sky_text}` - Temp: `{forecast.temperature}°C`\nLow: `{forecast.low}°C` - High: `{forecast.high}°C`\nPrecip: `{forecast.precip}`", inline=True)
 
         await channel.send("<@729135459405529118>", embed=embed)
         await client.close()
