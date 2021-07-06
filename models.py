aee174e0548465fe11edb13da2ac16e4e96bc188
zurgeg <zurgeg@gmail.com>
2021-07-06 18:14:53 -0400

    Apparently git didn't commit these

--- models.py
+++ models.py
@@ -1,15 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, Boolean
-from sqlalchemy.orm import declarative_base
-from os import getenv
-from dotenv import load_dotenv
-load_dotenv()
-db = create_engine(getenv('SQLALCHEMY_URI'))
-Model = declarative_base()
-
-class User(Model):
+from bot import db
+class User(db.Model):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    discord_id = Column(Integer, primary_key=True, nullable=False)
-    strikes = Column(Integer, default=0)
-    muted = Column(Boolean, default=False)
+    id = db.Column(Integer, primary_key=True)
+    discord_id = db.Column(Integer, primary_key=True, nullable=False)
+    strikes = db.Column(Integer, default=0)
+    muted = db.Column(Boolean, default=False)
