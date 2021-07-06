54ae1139754c1340bb91c1439059576b9ccb8bff
zurgeg <zurgeg@gmail.com>
2021-07-06 18:31:47 -0400

    [database] [models] Cleanliness updates

--- models.py
+++ models.py
@@ -2,7 +2,8 @@ from bot import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(Integer, primary_key=True)
-    discord_id = db.Column(Integer, primary_key=True, nullable=False)
-    strikes = db.Column(Integer, default=0)
-    muted = db.Column(Boolean, default=False)
+    id = db.Column(db.Integer, primary_key=True)
+    discord_id = db.Column(db.Integer, nullable=False)
+    strikes = db.Column(db.Integer, default=0)
+    muted = db.Column(db.Boolean, default=False)
+    guild = db.Column(db.Integer, nullable=False)
