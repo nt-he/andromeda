7a59378832a93a4d1344da7667038aab9b39c151
zurgeg <zurgeg@gmail.com>
2021-07-16 19:40:48 -0400

    Wait what

--- bot_types.py
+++ bot_types.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 class DbSession:
     def __init__(self, engine):
         self._engine = engine
-        self.session = sessionmaker(bind=self._engine)
+        self.session = sessionmaker(bind=self._engine)()
         self.Model = declarative_base()
         self.Column = Column
         self.Integer = Integer
