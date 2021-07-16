e4a303bb7bebd2115c95a82ab1a46122e0c82cda
zurgeg <zurgeg@gmail.com>
2021-07-16 13:40:44 -0400

    [web] Add a web UI

---
+++ web.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import os.path, os
+app = Flask(__name__)
+
+class Mii:
+    def __init__(self, did, file):
+        self.did = did
+        self.file = file
+    def get_discord_id(self):
+        return self.did
+    def get_file(self):
+        return self.file
+    def get_full_path(self):
+        return os.path.join("miis", self.file)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+@app.route("/load")
+def load_miis():
+        miis = [f for f in os.listdir("miis") if os.path.isfile(os.path.join("miis", f))]
+        if request.args:
+            page = int(request.args.get("page"))
+            if page == 0:
+                return {"miis": miis[0:10]}
+            elif page == len(miis):
+                return make_response(jsonify({}), 999) # I decided to pick this code because I had no clue about anything in the HTTP spec that would make sense.
+            else:
+                return {"miis": miis[page: page + 10]}
+        else:
+            return make_response({}, 400)
+        
