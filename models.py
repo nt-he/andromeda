8c612ef9ab434ec0a2c2ce41affb7e4c3075be7c
zurgeg <zurgeg@gmail.com>
2021-07-16 15:58:16 -0400

    Database updates

--- models.py
+++ models.py
@@ -7,3 +7,8 @@ class User(db.Model):
     strikes = db.Column(db.Integer, default=0)
     muted = db.Column(db.Boolean, default=False)
     guild = db.Column(db.Integer, nullable=False)
+class CachedUser(db.Model):
+    __tablename__ = "cached_users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), nullable=True)
+    discriminator = db.Column(db.String(4), nullable=True)
