84a649a7981e52be10adcb9d40cf6d4f6f50e37a
zurgeg <zurgeg@gmail.com>
2021-07-17 09:58:25 -0400

    bruh

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
-
+    __table_args__ = {'sqlite_autoincrement': True}
     id = db.Column(db.Integer, primary_key=True)
     discord_id = db.Column(db.Integer, nullable=False)
     strikes = db.Column(db.Integer, default=0)
@@ -12,6 +12,7 @@ class User(Base):
         
 class CachedUser(Base):
     __tablename__ = "cached_users"
+    __table_args__ = {'sqlite_autoincrement': True}
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), nullable=True)
     discriminator = db.Column(db.String(4), nullable=True)
