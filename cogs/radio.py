ee5cfd07eaf377b4781fc9bd0bb8c13273717186
zurgeg <zurgeg@gmail.com>
2021-07-19 14:13:48 -0400

    Add the ability to tune into live128 (whatever that is)

--- cogs/radio.py
+++ cogs/radio.py
@@ -1,27 +1,43 @@
+import discord
 from discord.ext import commands
 from os.path import join
+import requests
+from discord import FFmpegPCMAudio
+import asyncio
+from aiohttp import ClientSession
+import binascii
+class StreamingMP3Source(discord.AudioSource):
+    def __init__(self, url):
+        self.url = url
+        self.sess = requests.Session().get(self.url, stream=True)
+        print("'mogus")
+        super().__init__()
+    def read(self):
+        thingy = self.sess.raw.read(20)
+        return thingy
+    def is_opus(self):
+        return False
+    def cleanup(self):
+        print("'mogus III")
 
-from discord.player import FFmpegOpusAudio
 class Radio(commands.Cog):
     """Radio"""
     def __init__(self, bot):
         self.bot = bot
     
     @commands.command()
-    async def tune_in(self, ctx, station):
+    async def tune_in(self, ctx):
         """Tune in to a radio station"""
-        await ctx.send(f"Tuning in to {station}")
-        # Internally, stations are stored as xxx_x.mp3, rather than xxx.x, so we need to replace dots with underscores
-        station = station.replace('.', '_')
-        station = station.replace("/", "") # Delete any slashes for safety
         # Now we create an audio source:
-        src = FFmpegOpusAudio(source=join("radio", f"{station}.mp3"))
+        src = StreamingMP3Source("http://antenna5stream.neotel.mk:8000/live128")
         if ctx.author.voice is not None:
-            vc = await ctx.author.voice.channel.connect()
+            await ctx.author.voice.channel.connect()
         else:
             await ctx.send("You need to be in a voice channel to play a radio station")
             return
         # Now we need to play:
-        vc.play(src)
+        ctx.voice_client.play(src, after=lambda e: print('Player error: %s' % e) if e else None)
+
+
 def setup(bot):
     bot.add_cog(Radio(bot))
