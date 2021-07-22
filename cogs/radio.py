2e5e6a04cd741ccc5abc520404164a47f9984ac5
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-07-22 19:23:53 -0400

    Revert "radio moment"
    This reverts commit 9d34531608d29ed65cd317f156732e172729a21f.

--- cogs/radio.py
+++ cogs/radio.py
@@ -34,30 +34,15 @@ class Radio(commands.Cog):
         # Now we create an audio source:
         
         src = StreamingMP3Source("http://antenna5stream.neotel.mk:8000/live128")
-        if ctx.voice_client is None and ctx.author.voice is not None:
+        if ctx.author.voice is not None:
             await ctx.author.voice.channel.connect()
-        elif ctx.voice_client is not None:
-            pass # We're already in a voice channel
-        elif ctx.author.voice.channel is None:
+        else:
             await ctx.send("You need to be in a voice channel to play a radio station")
             return
-        elif ctx.voice_client is not None:
-            await ctx.voice.client.move_to(ctx.author.voice.channel)
-        else:
-            await ctx.send(f"""**A serious error has occured**
-Report this to <@{self.bot.owner_id}>, 
-and send them this information:
-> Line number: 41
-> Reason: Bottom of if else hit
-> Variables:
-> Author VC: {ctx.author.voice.channel.id}
-> Bot VC: {ctx.voice_client.channel.id}
-""")
-        await ctx.send("Source created.")
         # Now we need to play:
         ctx.voice_client.play(src, after=lambda e: print('Player error: %s' % e) if e else None)
     @commands.command()
-    async def tune_out(self, ctx):
+    async def stop(self, ctx):
         """Disconnect from the radio station"""
         ctx.voice_client.stop()
         await ctx.voice_client.disconnect()
